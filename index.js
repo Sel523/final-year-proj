632c607387935fcb0c893d6103fbd77d4a0989f6
sel523 <antzen21.SG@gmail.com>
2023-09-10 01:22:42 +0100

    added try-catch block to code
    changed loadfunctions function to have a trycatch block so that it outputs errors if it runs into one and did the same to the startbot function

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 require('dotenv').config();
 const variables = process.env;
 const { token } = variables;
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs').promises;
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 async function loadFunctions() {
-  const functionFolders = await fs.readdir('./functions');
-  for (const folder of functionFolders) {
-    const functionFiles = (await fs.readdir(`./functions/${folder}`)).filter(
-      (file) => file.endsWith('.js'),
-    );
-    for (const file of functionFiles) {
-      require(`./functions/${folder}/${file}`)(client);
+  try {
+    const functionFolders = await fs.readdir('./functions');
+    for (const folder of functionFolders) {
+      const functionFiles = (await fs.readdir(`./functions/${folder}`)).filter(
+        (file) => file.endsWith('.js'));
+      for (const file of functionFiles) {
+        require(`./functions/${folder}/${file}`)(client);
+      }
     }
+  } catch (err) {
+    console.error(`Error while loading functions: ${err}`);
   }
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(token);
+async function startBot() {
+  try {
+    await loadFunctions();
+    await client.login(token);
+  } catch (err) {
+    console.error(err);
+  }
+  client.handleEvents();
+  client.handleCommands();
+}
+
+startBot().catch(console.error);
