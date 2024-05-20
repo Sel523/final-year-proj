58d6a02e57954d3946374c02b35b787e17e0087f
sel523 <antzen21.SG@gmail.com>
2024-05-20 12:57:51 +0100

    Updated Message Moderation
    Made it so that message moderation now checks database to for the servers threshold settings. instead of fixed settings in the code

--- events/MessageModeration.js
+++ events/MessageModeration.js
@@ -1,7 +1,9 @@
-const {Events} = require('discord.js');
-const ContentSafetyClient = require("@azure-rest/ai-content-safety").default,
-  { isUnexpected } = require("@azure-rest/ai-content-safety");
-const { AzureKeyCredential } = require("@azure/core-auth");
+const { Events, EmbedBuilder } = require('discord.js');
+const ContentSafetyClient = require('@azure-rest/ai-content-safety').default;
+const { isUnexpected } = require('@azure-rest/ai-content-safety');
+const { AzureKeyCredential } = require('@azure/core-auth');
+const db = require('../services/db');
+const { defaultTextViolationThresholds } = require('../config/thresholds');
 
 
 module.exports = {
@@ -9,32 +11,41 @@ module.exports = {
   once: false,
   async execute(msg) {
     if (msg.author.bot) {
-      return
+      return;
     }
     if (msg.content.length <= 0) {
-      return
+      return;
     }
-    const msgViolation = await analyzeMessageText(msg.content);
+
+    const thresholds = await getTextThresholds(msg.guild.id);
+    const msgViolation = await analyzeMessageText(msg.content, thresholds);
 
     if (msgViolation.block !== true) {
-      return
+      return;
     }
     await msg.delete();
-    await msg.channel.send(`A message has been deleted due to it violating these rules: ${msgViolation.violations.join(', ')}`)
+    await msg.channel.send(`A message has been deleted due to it violating these rules: ${msgViolation.violations.join(', ')}`);
+
+    const logChannel = await db.query('SELECT channel_id FROM log_channels WHERE guild_id=?', [msg.guild.id]);
+    if (logChannel.length > 0) {
+      const channel = await msg.client.channels.fetch(logChannel[0].channel_id);
+
+      const embed = new EmbedBuilder()
+        .setTitle('AI Text Detection')
+        .addFields(
+          {name: 'Message Author', value: `<@${msg.author.id}>`},
+          {name: 'Message Violations', value: msgViolation.violations.join(', ') },
+          {name: 'Message Content', value: msg.content },
+        )
+
+      await channel.send({embeds: [embed]});
+    }
   },
 };
 
-const textViolationThresholds = {
-  'Hate': 3,
-  'Sexual': 3,
-  'Violence': 4,
-  'SelfHarm': 4
-}
-const combinedViolationThreshold = 4
-
-async function analyzeMessageText(text) {
-  const endpoint = process.env["azureContentSafetyEndpoint"] || "<endpoint>";
-  const key = process.env["azureContentSafetyApiKey"] || "<key>";
+async function analyzeMessageText(text, thresholds) {
+  const endpoint = process.env['azureContentSafetyEndpoint'] || '<endpoint>';
+  const key = process.env['azureContentSafetyApiKey'] || '<key>';
 
   const credential = new AzureKeyCredential(key);
   const client = ContentSafetyClient(endpoint, credential);
@@ -42,28 +53,45 @@ async function analyzeMessageText(text) {
   const analyzeTextOption = { text: text };
   const analyzeTextParameters = { body: analyzeTextOption };
 
-  const result = await client.path("/text:analyze").post(analyzeTextParameters);
+  const result = await client.path('/text:analyze').post(analyzeTextParameters); // analyse the text
 
   if (isUnexpected(result)) {
     throw result;
   }
 
-  let violations = [];
-  let totalViolations = [];
+  const violations = []; // create an array to store the violations
+  const totalViolations = []; //  create an array to store the total violations
   let totalSeverity = 0;
 
   for (let category of result.body.categoriesAnalysis) {
-    if (category.severity >= textViolationThresholds[category.category]) {
-      violations.push(category.category)
+    if (category.severity >= thresholds[category.category]) {
+      violations.push(category.category); // if the severity of the category is greater than the threshold for the category add it to the violations array
     }
     if (category.severity > 0) {
-      totalViolations.push(category.category)
+      totalViolations.push(category.category);
     }
-    totalSeverity += category.severity
+    totalSeverity += category.severity; // add the severity of the category to the total severity
   }
 
   return {
-    block: (violations.length > 0) || (totalSeverity >= combinedViolationThreshold),
-    violations: totalViolations
-  }
+    block: (violations.length > 0) || (totalSeverity >= thresholds['combined']),
+    violations: totalViolations, // return the violations
+  };
+}
+
+async function getTextThresholds(guild_id) {
+  const thresholds = await db.query('SELECT name, value FROM text_detection_thresholds WHERE guild_id=?', [guild_id]); // get the text detection thresholds for the guild
+
+  const hateThreshold = thresholds.find(obj => obj.name === 'Hate')?.value ?? defaultTextViolationThresholds['Hate']; // get the hate text threshold for the guild
+  const sexualThreshold = thresholds.find(obj => obj.name === 'Sexual')?.value ?? defaultTextViolationThresholds['Sexual'];
+  const violenceThreshold = thresholds.find(obj => obj.name === 'Violence')?.value ?? defaultTextViolationThresholds['Violence'];
+  const selfHarmThreshold = thresholds.find(obj => obj.name === 'SelfHarm')?.value ?? defaultTextViolationThresholds['SelfHarm'];
+
+  return { // return the thresholds
+    Hate: hateThreshold,
+    Sexual: sexualThreshold,
+    Violence: violenceThreshold,
+    SelfHarm: selfHarmThreshold,
+    combined: Math.ceil((hateThreshold + sexualThreshold + violenceThreshold + selfHarmThreshold) / 4),
+  };
 }
